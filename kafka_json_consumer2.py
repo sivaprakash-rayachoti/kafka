b349c639b0d678a7ffb4cd7f79566b16b01c1660
sivaprakash-rayachoti <106383930+sivaprakash-rayachoti@users.noreply.github.com>
2022-11-21 17:40:18 +0530

    Add files via upload

---
+++ kafka_json_consumer2.py
@@ -0,0 +1,97 @@
+import argparse
+
+from confluent_kafka import Consumer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.json_schema import JSONDeserializer
+
+
+API_KEY = 'FXBQOYMN6BPLQATD'
+ENDPOINT_SCHEMA_URL  = 'https://psrc-mw0d1.us-east-2.aws.confluent.cloud'
+API_SECRET_KEY = 'DfRYczELBQNScQ8KomEyhH51nif/knENFjGMGAmUMslFRzzr4TroJ21DCdCynOro'
+BOOTSTRAP_SERVER = 'pkc-ymrq7.us-east-2.aws.confluent.cloud:9092'
+SECURITY_PROTOCOL = 'SASL_SSL'
+SSL_MACHENISM = 'PLAIN'
+SCHEMA_REGISTRY_API_KEY = 'TBNO7C6GKSK3Q2ZD'
+SCHEMA_REGISTRY_API_SECRET = '4ZrAK4hsnPqFBSitTlJ4aqKBX/TJEuCPItHbyz6WnddplTeZMcynE/fZw50HPH5S'
+
+
+def sasl_conf():
+
+    sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
+                 # Set to SASL_SSL to enable TLS support.
+                #  'security.protocol': 'SASL_PLAINTEXT'}
+                'bootstrap.servers':BOOTSTRAP_SERVER,
+                'security.protocol': SECURITY_PROTOCOL,
+                'sasl.username': API_KEY,
+                'sasl.password': API_SECRET_KEY
+                }
+    return sasl_conf
+
+
+
+def schema_config():
+    return {'url':ENDPOINT_SCHEMA_URL,
+    
+    'basic.auth.user.info':f"{SCHEMA_REGISTRY_API_KEY}:{SCHEMA_REGISTRY_API_SECRET}"
+
+    }
+
+
+class Order:   
+    def __init__(self,record:dict):
+        for k,v in record.items():
+            setattr(self,k,v)
+        
+        self.record=record
+   
+    @staticmethod
+    def dict_to_order(data:dict,ctx):
+        return Order(record=data)
+
+    def __str__(self):
+        return f"{self.record}"
+
+
+def main(topic):
+
+    schema_registry_conf = schema_config()
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    schema_str = schema_registry_client.get_latest_version('rest-topic-value').schema.schema_str
+    json_deserializer = JSONDeserializer(schema_str,
+                                         from_dict=Order.dict_to_order)
+
+    consumer_conf = sasl_conf()
+    consumer_conf.update({
+                     'group.id': 'group1',
+                     'auto.offset.reset': "earliest"})
+
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe([topic])
+
+    count = 0
+    while True:
+        try:
+            # SIGINT can't be handled when polling, limit timeout to 1 second.
+            msg = consumer.poll(1.0)
+            
+            if msg is None:
+                print('No of records consumed by consumer 2 :' + str(count))
+                
+                continue
+
+            order = json_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+
+            if order is not None:
+                print("User record {}: order: {}\n"
+                      .format(msg.key(), order))
+                count += 1
+
+        except KeyboardInterrupt:
+            break
+
+    consumer.close()
+
+main("rest-topic")
+
+# python /Users/DELL/Desktop/BigData/kafka/assignment/kafka_json_consumer2.py
---
+++ kafka_json_producer.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Confluent Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# A simple example demonstrating use of JSONSerializer.
+
+import argparse
+from uuid import uuid4
+from six.moves import input
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.json_schema import JSONSerializer
+#from confluent_kafka.schema_registry import *
+import pandas as pd
+from typing import List
+
+FILE_PATH = "/Users/DELL/Desktop/BigData/Kafka/Assignment/restaurant_orders.csv"
+columns=['order_number', 'order_date', 'item_name', 'quantity', 'product_price', 'total_products']
+
+API_KEY = 'FXBQOYMN6BPLQATD'
+ENDPOINT_SCHEMA_URL  = 'https://psrc-mw0d1.us-east-2.aws.confluent.cloud'
+API_SECRET_KEY = 'DfRYczELBQNScQ8KomEyhH51nif/knENFjGMGAmUMslFRzzr4TroJ21DCdCynOro'
+BOOTSTRAP_SERVER = 'pkc-ymrq7.us-east-2.aws.confluent.cloud:9092'
+SECURITY_PROTOCOL = 'SASL_SSL'
+SSL_MACHENISM = 'PLAIN'
+SCHEMA_REGISTRY_API_KEY = 'TBNO7C6GKSK3Q2ZD'
+SCHEMA_REGISTRY_API_SECRET = '4ZrAK4hsnPqFBSitTlJ4aqKBX/TJEuCPItHbyz6WnddplTeZMcynE/fZw50HPH5S'
+
+
+def sasl_conf():
+
+    sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
+                 # Set to SASL_SSL to enable TLS support.
+                #  'security.protocol': 'SASL_PLAINTEXT'}
+                'bootstrap.servers':BOOTSTRAP_SERVER,
+                'security.protocol': SECURITY_PROTOCOL,
+                'sasl.username': API_KEY,
+                'sasl.password': API_SECRET_KEY
+                }
+    return sasl_conf
+
+
+
+def schema_config():
+    return {'url':ENDPOINT_SCHEMA_URL,
+    
+    'basic.auth.user.info':f"{SCHEMA_REGISTRY_API_KEY}:{SCHEMA_REGISTRY_API_SECRET}"
+
+    }
+
+
+class Order:   
+    def __init__(self,record:dict):
+        for k,v in record.items():
+            setattr(self,k,v)
+        
+        self.record=record
+   
+    @staticmethod
+    def dict_to_order(data:dict,ctx):
+        return Order(record=data)
+
+    def __str__(self):
+        return f"{self.record}"
+
+
+def get_order_instance(file_path):
+    df=pd.read_csv(file_path)
+    orders:List[Order]=[]
+    for data in df.values:
+        order=Order(dict(zip(columns,data)))
+        orders.append(order)
+        yield order
+
+def order_to_dict(order:Order, ctx):
+    """
+    Returns a dict representation of a User instance for serialization.
+    Args:
+        user (User): User instance.
+        ctx (SerializationContext): Metadata pertaining to the serialization
+            operation.
+    Returns:
+        dict: Dict populated with user attributes to be serialized.
+    """
+
+    # User._address must not be serialized; omit from dict
+    return order.record
+
+
+def delivery_report(err, msg):
+    """
+    Reports the success or failure of a message delivery.
+    Args:
+        err (KafkaError): The error that occurred on None on success.
+        msg (Message): The message that was produced or failed.
+    """
+
+    if err is not None:
+        print("Delivery failed for User record {}: {}".format(msg.key(), err))
+        return
+    print('User record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+
+
+def main(topic):
+
+    
+    schema_registry_conf = schema_config()
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    schema_str = schema_registry_client.get_latest_version('rest-topic-value').schema.schema_str
+    string_serializer = StringSerializer('utf_8')
+    json_serializer = JSONSerializer(schema_str, schema_registry_client, order_to_dict)
+
+    producer = Producer(sasl_conf())
+        
+    print("Producing user records to topic {}. ^C to exit.".format(topic))
+    #while True:
+        # Serve on_delivery callbacks from previous calls to produce()
+    producer.poll(0.0)
+    try:
+        for order in get_order_instance(file_path=FILE_PATH):
+
+            print(order)
+            producer.produce(topic=topic,
+                            key=string_serializer(str(uuid4()), order_to_dict),
+                            value=json_serializer(order, SerializationContext(topic, MessageField.VALUE)),
+                            on_delivery=delivery_report)
+            
+    except KeyboardInterrupt:
+        pass
+    except ValueError:
+        print("Invalid input, discarding record...")
+        pass
+
+    print("\nFlushing records...")
+    producer.flush()
+
+main("rest-topic")
+
+##python /Users/DELL/Desktop/BigData/kafka/assignment/kafka_json_producer.py
